2ff8721ea53300c81ee2f0f0ba8fa8ef0bd4c31d
Leonardo Fonseca <leonardorodriguesfonseca@gmail.com>
2026-02-14 22:43:47 -0300

    add apple watch app

---
+++ ios/Watch Watch App/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  Watch Watch App
+//
+//  Created by Leonardo Fonseca on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
